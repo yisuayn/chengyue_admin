fcd2167f76a344c3d4e2b3701ca2ed0776da2955
yisuayn <1572320149qq.com>
2025-03-13 16:06:09 +0800

    全局调用axios

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import Cyecharts from "./components/Cyecharts.vue";
 import Cybarchart from "./components/Cybarchart.vue";
 import Cypiechart from "./components/Cypiechart.vue";
 
+
 // 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
 import * as echarts from 'echarts/core';
 // 引入柱状图图表，图表后缀都为 Chart
@@ -26,6 +27,9 @@ import {
 import { LabelLayout, UniversalTransition } from 'echarts/features';
 // 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
 import { CanvasRenderer } from 'echarts/renderers';
+
+import axios from 'axios'
+Vue.prototype.$axios = axios
 
 Vue.use(ElementUI);
 echarts.use([
