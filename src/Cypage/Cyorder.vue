3dfc8f30d374f7b0bc309382ffdb4690bade4a1d
yisuayn <1572320149qq.com>
2025-04-14 17:39:18 +0800

    抽屉组件完成

--- src/Cypage/Cyorder.vue
+++ src/Cypage/Cyorder.vue
@@ -1,22 +1,81 @@
 <template>
   <div>
-    <el-button @click="handleclick()">打开</el-button>
-    
-    <CustomDrawer :drawer="drawerVisible"></CustomDrawer>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column fixed prop="date" label="日期" width="150"></el-table-column>
+      <el-table-column prop="name" label="姓名"></el-table-column>
+      <el-table-column prop="province" label="省份"></el-table-column>
+      <el-table-column prop="city" label="市区"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table-column prop="zip" label="邮编" ></el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button @click="openDrawer(scope.row)" type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <CustomDrawer
+      v-model="drawerVisible"
+      title="查看用户"
+      :form="form"
+      :rules="rules"
+      @confirm="handleSubmit"
+      @cancel="handleCancel"
+    />
   </div>
 </template>
 
 <script>
 export default {
+  components: {},
   data() {
     return {
-      drawerVisible:"",
-    }
+      tableData: [
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1517 弄",
+          zip: 200333,
+        },
+      ],
+      drawerVisible: false,
+      form: {
+        name: "",
+        date: "",
+        province: "",
+        city: "",
+        address: "",
+        zip: "",
+      },
+      rules: {
+        name: [{ required: true, message: "请输入姓名", trigger: "blur" }],
+        date: [{ required: true, message: "请输入姓名", trigger: "blur" }],
+        province: [{ required: true, message: "请输入姓名", trigger: "blur" }],
+        city: [{ required: true, message: "请输入姓名", trigger: "blur" }],
+        address: [{ required: true, message: "请输入姓名", trigger: "blur" }],
+        email: [
+          { required: true, message: "请输入邮箱", trigger: "blur" },
+          { type: "email", message: "邮箱格式不正确", trigger: "blur" },
+        ],
+      },
+    };
   },
-  methods:{
-    handleclick(){
-      this.showDrawer = true;
-    }
-  }
-}
+  methods: {
+    openDrawer(row) {
+      this.drawerVisible = true;
+      this.form = row;
+    },
+    handleSubmit(formData) {
+      console.log("提交数据：", formData);
+      this.$message.success("提交成功！");
+      // 可以在这里发起 API 请求
+    },
+    handleCancel() {
+      this.$message.info("已取消");
+    },
+  },
+};
 </script>
--- src/components/CustomDrawer.vue
+++ src/components/CustomDrawer.vue
@@ -1,48 +1,125 @@
-<!-- SimpleDrawer.vue -->
 <template>
   <el-drawer
+    :visible.sync="visible"
     :title="title"
-    :visible.sync="localVisible"
-    :direction="direction"
     :size="size"
+    :direction="direction"
+    :before-close="handleClose"
+    :custom-class="customClass"
+    :destroy-on-close="destroyOnClose"
   >
-  <span>我来了</span>
+    <el-form ref="form" :model="formData" :rules="rules" :label-width="labelWidth">
+      <el-form-item label="姓名" prop="name">
+        <el-input v-model="formData.name" placeholder="请输入姓名"></el-input>
+      </el-form-item>
+      <el-form-item label="日期" prop="date">
+        <el-input v-model="formData.date" placeholder="完成日期"></el-input>
+      </el-form-item>
+      <el-form-item label="省份" prop="province">
+        <el-input v-model="formData.province" placeholder="完成日期"></el-input>
+      </el-form-item>
+      <el-form-item label="城市" prop="city">
+        <el-input v-model="formData.city" placeholder="完成日期"></el-input>
+      </el-form-item>
+      <el-form-item label="地址" prop="address">
+        <el-input v-model="formData.address" placeholder="完成日期"></el-input>
+      </el-form-item>
+      <el-form-item label="邮编" prop="zip">
+        <el-input v-model="formData.zip" placeholder="完成日期"></el-input>
+      </el-form-item>
+    </el-form>
+
+    <div class="drawer-footer">
+      <el-button @click="handleCancel">取 消</el-button>
+      <el-button type="primary" @click="handleConfirm">确 定</el-button>
+    </div>
   </el-drawer>
 </template>
 
 <script>
 export default {
-  name: 'CustomDrawer',
+  name: "FormDrawer",
   props: {
-    visible: {
+    value: {
       type: Boolean,
-      default: false
+      default: false,
     },
     title: {
       type: String,
-      default: '抽屉标题'
+      default: "表单抽屉",
+    },
+    size: {
+      type: [String, Number],
+      default: "35%",
     },
     direction: {
       type: String,
-      default: 'rtl' // 支持：rtl / ltr / ttb / btt
+      default: "rtl",
     },
-    size: {
+    customClass: {
       type: String,
-      default: '30%' // 支持像素：'300px' 或百分比
-    }
+      default: "",
+    },
+    destroyOnClose: {
+      type: Boolean,
+      default: true,
+    },
+    labelWidth: {
+      type: String,
+      default: "80px",
+    },
+    form: {
+      type: Object,
+      default: () => ({}),
+    },
+    rules: {
+      type: Object,
+      default: () => ({}),
+    },
   },
   data() {
     return {
-      localVisible: this.visible
-    }
+      visible: this.value,
+      formData: {},
+    };
   },
   watch: {
-    visible(newVal) {
-      this.localVisible = newVal
+    value(val) {
+      this.visible = val;
+      if (val) {
+        // 打开时拷贝父组件传来的表单数据
+        this.formData = JSON.parse(JSON.stringify(this.form));
+      }
     },
-    localVisible(newVal) {
-      this.$emit('update:visible', newVal)
-    }
-  }
-}
+    visible(val) {
+      this.$emit("input", val);
+    },
+  },
+  methods: {
+    handleClose(done) {
+      this.$emit("close");
+      done();
+    },
+    handleCancel() {
+      this.visible = false;
+      this.$emit("cancel");
+    },
+    handleConfirm() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          this.$emit("confirm", this.formData);
+          this.visible = false;
+        }
+      });
+    },
+  },
+};
 </script>
+
+<style scoped>
+.drawer-footer {
+  text-align: right;
+  padding: 10px 0;
+  border-top: 1px solid #ebeef5;
+}
+</style>
